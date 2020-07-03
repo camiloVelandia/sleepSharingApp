dcd8280c969e71a8148120426f5992829b924a88
Stiven Jimenez <sajqprogramming@gmail.com>
2020-07-02 19:49:11 -0500

    styled-components installed and global styles ready

--- src/container/App.jsx
+++ src/container/App.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import '../styles/main.scss';
+import  GlobalStyle  from '../GlobalStyles'
+
+import  Header  from '../components/Header'
 
 const App = () => {
-  return <>Buscando roomie</>;
+  return (
+    <>
+      <GlobalStyle />
+      <Header />
+    </>
+  );
 };
 
 export default App;
