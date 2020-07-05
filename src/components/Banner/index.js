eb9034822874a372c945703e3eda01f6ad984f94
camilo <velandiacamilo356@gmail.com>
2020-07-04 23:23:00 -0500

    ADD RegisterRoom component

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Figure } from './styles';
-import BannerImage from '../../../static/hero_image.png';
 
 const Banner = () => {
   return (
--- src/container/App.jsx
+++ src/container/App.jsx
@@ -7,8 +7,9 @@ import Hero from '../components/Hero';
 import Prom from '../components/Prom';
 import Contact from '../components/Contact';
 import Footer from '../components/Footer';
+import RegisterRoom from '../components/RegisterRoom';
 // import Banner from '../components/Banner';
-import Cards from '../components/Cards';
+// import Cards from '../components/Cards';
 // import Sppiner from '../general/Sppiner'
 // import PageNotFound from '../general/ PageNotFound'
 
@@ -17,12 +18,15 @@ const App = () => {
     <>
       <GlobalStyle />
       <Header />
+
       <Hero />
       {/* <Cards /> */}
       {/* <PageNotFound /> */}
       <Prom />
       {/* <Banner /> */}
       <Contact />
+      <RegisterRoom />
+
       <Footer />
       {/* <Sppiner /> */}
     </>
