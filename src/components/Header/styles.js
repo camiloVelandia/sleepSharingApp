8fe5ece5c17c17f52d06fd62c314604858f752f1
camilo <velandiacamilo356@gmail.com>
2020-07-05 21:40:10 -0500

    Update responsive in hero and header

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,37 +1,52 @@
 import styled from 'styled-components';
 
-export const TopNavbar =styled.header`
-  height:60px;
-  background-color:#114B5F;
-  display:flex;
-  align-items:center;
-`
+export const TopNavbar = styled.header`
+  height: 60px;
+  background-color: #114b5f;
+  display: flex;
+  align-items: center;
+`;
 
 export const Menu = styled.nav`
-  display:flex;
-  justify-content:space-between;
-  width:100%;
-`
-
-export const Ul=styled.ul`
-  display:flex;
-  align-items:center;
-  & li{
-    list-style:none;
-    color:#DADADA;
-    margin-right:20px;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  @media (max-width: 767px) {
+    display: none;
   }
-`
+`;
 
-export const A =styled.a`
-  background-color:#F8F8F8;
-  color:#114B5F;
-  border-radius:4px;
-  padding:10px;
-  text-decoration:none;
-`
-export const Img=styled.img `
-  width:160;
-  height:40px;
-  margin-right:20px;
-`
+export const Ul = styled.ul`
+  display: flex;
+  align-items: center;
+  & li {
+    list-style: none;
+    color: #dadada;
+    margin-right: 20px;
+  }
+`;
+
+export const A = styled.a`
+  background-color: #f8f8f8;
+  color: #114b5f;
+  border-radius: 4px;
+  padding: 10px;
+  text-decoration: none;
+`;
+export const Img = styled.img`
+  width: 160;
+  height: 40px;
+  margin-right: 20px;
+`;
+export const MenuResponsive = styled.div`
+  font-size: 30px;
+  position: absolute;
+  right: 0;
+  & a {
+    color: white;
+    padding: 0 20px;
+  }
+  @media (min-width: 767px) {
+    display: none;
+  }
+`;
--- src/components/Hero/styles.js
+++ src/components/Hero/styles.js
@@ -1,67 +1,87 @@
-import styled from 'styled-components'
-import  heroImage from '../../../static/hero_image.png'
-
+import styled from 'styled-components';
+import heroImage from '../../../static/hero_image.png';
 
 export const HeroSearch = styled.div`
-  background-image:url(${heroImage});
-  background-repeat:no-repeat;
-  background-size:cover;
-  background-position:center center;
-  height:700px;
-  display:flex;
-  flex-direction:column;
-  align-items:center;
-  justify-content:space-around;
-  & h1{
-    text-shadow: 2px 2px #777777;;
-    color:#F8F8F8;
-    text-align:center;
+  background-image: url(${heroImage});
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  height: 90vh;
+  & h1 {
+    text-shadow: 2px 2px #777777;
+    color: #f8f8f8;
+    text-align: center;
   }
-  & h2{
-    text-shadow: 2px 2px #777777;;
-    color:#F8F8F8;
-    text-align:center;
-    font-size:42px;
+  & h2 {
+    text-shadow: 2px 2px #777777;
+    color: #f8f8f8;
+    text-align: center;
+    font-size: 42px;
   }
-`
+  @media (max-width: 767px) {
+    height: 100vh;
+    & h1,
+    h2 {
+      font-size: 26px;
+    }
+  }
+`;
 
-
-export const Search= styled.div`
-  border-radius:20px;
-  height:39px;
-  width:481px;
-  background-color:#F8F8F8;
-  display:flex;
-  justify-content:center;
-  align-items:center;
-  & input{
-    border:none;
-    height:30px;
-    width:430px;
+export const Search = styled.div`
+  border-radius: 20px;
+  height: 39px;
+  width: 100%;
+  max-width: 481px;
+  background-color: #f8f8f8;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  & input {
+    border: none;
+    height: 30px;
+    width: 100%;
+    max-width: 430px;
   }
-  & input:focus{
-    outline:none;
-    outline-width:0;
+  & input:focus {
+    outline: none;
+    outline-width: 0;
   }
-`
-export const ButtonContainer =styled.div`
-  display:flex;
-  width:80vw;
-  justify-content:space-around;
-  & a{
-    display:flex;
-    align-items:center;
-    border:#028090 solid 1px;
-    height:41px;
-    width:330px;
-    justify-content:center;
-    background-color:#F8F8F8;
-    color:#028090;
-    text-decoration:none;
-    border-radius:8px;
+  @media (max-width: 767px) {
+    width: 90%;
+    & input {
+      width: 90%;
+    }
   }
-  & a.host__button{
-    color:#F8F8F8;
-    background-color:#028090;
+`;
+export const ButtonContainer = styled.div`
+  display: flex;
+  width: 80vw;
+  justify-content: space-around;
+  & a {
+    display: flex;
+    align-items: center;
+    border: #028090 solid 1px;
+    height: 41px;
+    width: 330px;
+    justify-content: center;
+    background-color: #f8f8f8;
+    color: #028090;
+    text-decoration: none;
+    border-radius: 8px;
+    margin-bottom: 60px;
   }
-`
+  & a.host__button {
+    color: #f8f8f8;
+    background-color: #028090;
+  }
+  @media (max-width: 767px) {
+    flex-wrap: wrap;
+    & a {
+      margin-bottom: 10px;
+    }
+  }
+`;
