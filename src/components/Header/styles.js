a4e62095cc04ebc910f14b01e889632c81fc30ca
Lennin666 <soporte@allnexus.com.co>
2020-07-09 20:48:39 -0500

    menu responsive

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -15,6 +15,7 @@ export const Menu = styled.nav`
     color: white;
     text-decoration: none;
   }
+
   @media (max-width: 767px) {
     display: none;
   }
@@ -23,6 +24,7 @@ export const Menu = styled.nav`
     border: none;
     color: white;
     cursor: pointer;
+    
   }
 `;
 
@@ -42,13 +44,41 @@ export const Img = styled.img`
   margin-left:20px;
 `;
 export const MenuResponsive = styled.div`
-  font-size: 30px;
+  display:flex;
   position: absolute;
-  right: 0;
-  & a {
-    color: white;
-    padding: 0 20px;
+  top: 0px;
+  right: 30px;
+  font-size: 16px;
+  height: 60px;
+  width: 160px;
+  align-items: center;
+  justify-content: space-around;
+  
+  & Ul { 
+  display: none;
+  position: absolute;
+  flex-direction:column;
+  list-style: none;
+  margin: 57px 0px 0px 0px;
+  padding: 0px;
+  width: 200px;
+  z-index: 1;
+  text-decoration: none;
   }
+  & i {
+    color: white;
+    
+  }
+
+  &:hover > Ul{
+    display:flex;
+    flex-direction: column;
+    text-align: right;
+    background-color: rgba(0,0,0, 0.7);
+  }
+
+
+
   @media (min-width: 767px) {
     display: none;
   }
