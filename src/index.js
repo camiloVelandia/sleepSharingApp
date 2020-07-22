0d8b1a40113f6dde038e90988279a92be9752d9c
stivenjimenez <sajqprogramming@gmail.com>
2020-07-22 05:10:51 -0500

    images to firebase

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as firebase from 'firebase/app';
+import firebase from './firebase'
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import reduxThunk from 'redux-thunk'
@@ -8,16 +8,10 @@ import reducers from './reducers';
 import App from './container/App';
 
 
-firebase.initializeApp({
-  apiKey: 'AIzaSyAzQ_pZ8Kcqth33V6Dd03J2j9GLgvKrUA4',
-  authDomain: 'pseudogram-ea422.firebaseapp.com',
-  databaseURL: 'https://pseudogram-ea422.firebaseio.com',
-  projectId: 'pseudogram-ea422',
-  storageBucket: 'pseudogram-ea422.appspot.com',
-  messagingSenderId: '79513015735',
-  appId: '1:79513015735:web:629dc9114dc98c2da2c432',
-  measurementId: 'G-W3PFXFKRKF',
-});
+
+
+
+
 
 
 const store = createStore(
